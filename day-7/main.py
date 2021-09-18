b7c3c941e65a34b12ef077b41f1608fc15ed2ec3
CodeRed30 <katrina@revmo.app>
2021-09-19 00:01:16 +0100

    Section 2

--- day-7/main.py
+++ day-7/main.py
@@ -14,10 +14,6 @@ print(chosen_word)
 print(guess)
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-if guess in chosen_word:
-    print(True)
-else:
-    print(False)
 
 
 # TODO-1: - Create an empty List called display.
@@ -27,5 +23,22 @@ else:
 display = []
 for each in range(len(chosen_word)):
     display.append("_")
+
+print(display)
+
+# TODO-2: - Loop through each position in the chosen_word;
+# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+
+count = 0
+for char in chosen_word:
+    if guess == char:
+        display[count] = guess
+        count += 1
+    else:
+        count += 1
+
+# TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+# Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 
 print(display)
