3558f845930961598428a94f2c66fe5be7b2c1e5
CodeRed30 <katrina@revmo.app>
2021-09-20 14:53:42 +0100

    While loop

--- day-7/main.py
+++ day-7/main.py
@@ -1,44 +1,42 @@
 import random
 
 # Step 1
-word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-random_number = random.randint(0, len(word_list)-1)
-chosen_word = word_list[random_number]
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Choose a letter: \n").lower()
-
-print(chosen_word)
-print(guess)
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-
 
 # TODO-1: - Create an empty List called display.
 # For each letter in the chosen_word, add a "_" to 'display'.
 # So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
-display = []
-for each in range(len(chosen_word)):
-    display.append("_")
-
-print(display)
-
 # TODO-2: - Loop through each position in the chosen_word;
 # If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 # e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 
-count = 0
-for char in chosen_word:
-    if guess == char:
-        display[count] = guess
-        count += 1
-    else:
-        count += 1
-
 # TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 # Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 
-print(display)
+# TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+
+word_list = ["aardvark", "baboon", "camel"]
+
+random_number = random.randint(0, len(word_list)-1)
+chosen_word = word_list[random_number]
+
+display = []
+for each in range(len(chosen_word)):
+    display.append("_")
+
+while "_" in display:
+    guess = input("Choose a letter:  ").lower()
+    count = 0
+    for char in chosen_word:
+        if guess == char:
+            display[count] = guess
+            count += 1
+        else:
+            count += 1
+    print(display)
